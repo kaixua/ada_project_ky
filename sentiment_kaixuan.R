829254c5fcfd8cbf6659fb0aafc25cfdce91ecce
kaixua <42948817+kaixua@users.noreply.github.com>
2018-12-05 22:23:28 -0700

    Update sentiment_kaixuan.R

--- sentiment_kaixuan.R
+++ sentiment_kaixuan.R
@@ -1,10 +1,14 @@
 library(ggplot2)
+#### read in files
 pos <- read.csv("sentiment score_pos.csv")
 neg <- read.csv("sentiment score_neg.csv")
-qplot(pos$score, geom="histogram")
-qplot(neg$score, geom="histogram")
+##### assign headers
 names(pos) <- c("score","counter")
 names(neg) <- c("score","counter")
+#### histogram for scores
+qplot(pos$score, geom="histogram")
+qplot(neg$score, geom="histogram")
+#### do the percentage calculation
 score_pos <- sum(pos$score)
 score_neg <- sum(neg$score)
 total <- abs(score_neg) + score_pos
